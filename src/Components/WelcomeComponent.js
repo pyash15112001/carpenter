b7dd0bd9d002a50a5c83be16c7cbff8d772331e4
pyash15112001 <128290413+pyash15112001@users.noreply.github.com>
2023-03-29 14:31:27 +0530

    Add files via upload

---
+++ src/Components/WelcomeComponent.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const WelcomeComponent = () => {
+  return (
+    <h1 className='text-center text-danger mt-5'>Welcome to our site</h1>
+  )
+}
+
+export default WelcomeComponent
